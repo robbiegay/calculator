ce90e445e002f076ccada1fe8f3ef89fb67c48f8
Robbie Gay <robbiecgay@gmail.com>
2019-09-25 20:44:31 -0400

    Tried some more dynamic rendering

--- js/main.js
+++ js/main.js
@@ -2,32 +2,36 @@
 
 function loadCalc() {
     function renderElement(element, classes) {
-        let temp = document.createElement(element);
-        temp.className = classes;
-        return temp;
+        let output = document.createElement(element);
+        output.className = classes;
+        return output;
     }
 
+    // Create Elements
     let container = renderElement('div', 'container-fluid');
+    let row = renderElement('row');
 
-    // let container = document.createElement('div');
-    // container.className = 'container-fluid';
+    // let leftCol = renderElement('div', 'col-0 col-sm-1 col-md-2 col-lg-3 bg-warning');
+    // let centerCol = renderElement('div', 'col-12 col-sm-10 col-md-8 col-lg-6 text-center bg-success');
+    // let rightCol = renderElement('div', 'col-0 col-sm-1 col-md-2 col-lg-3 bg-warning');
 
-    let row = document.createElement('row');
+    let leftCol = renderElement('div', 'col bg-warning');
+    let centerCol = renderElement('div', 'col text-center bg-success');
+    let rightCol = renderElement('div', 'col bg-warning');
 
-    let leftCol = document.createElement('div');
-    leftCol.className = 'col-0 col-sm-1 col-md-2 col-lg-3';
-
-    let centerCol = document.createElement('div');
-    centerCol.className = 'col-12 col-sm-10 col-md-8 col-lg-6 text-center';
-
-    let title = document.createElement('h1');
-    title.className = 'my-5 display-4';
+    let title = renderElement('h1', 'my-5 display-4');
     title.innerHTML = 'Calculator';
 
-    let rightCol = document.createElement('div');
-    rightCol.className = 'col-0 col-sm-1 col-md-2 col-lg-3';
+    let calc = renderElement('table', 'bg-light');
+    calc.innerHTML = '<tr class="table"><th>0</th></tr><tr><td>test</td></tr>';
 
+    let testP = renderElement('p', 'bg-warning');
+    testP.innerHTML = 'this is only a test.';
+
+    // Append Elements
     centerCol.appendChild(title);
+    centerCol.appendChild(calc);
+    centerCol.appendChild(testP);
 
     row.appendChild(rightCol);
     row.appendChild(centerCol);
@@ -38,5 +42,25 @@ function loadCalc() {
     let app = document.getElementById('app');
     app.appendChild(container);
 
+    createBoard(20);
+
 }
 
+
+function createBoard(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        let div = document.createElement('div');
+        div.className += 'col-3 border bg-white display-2';
+        div.id = `tile-${i}`;
+        div.setAttribute('style', 'height: 40px;');
+        let text = document.createTextNode('_');
+        div.appendChild(text);
+        div.addEventListener('click', clickedOn);
+        document.body.appendChild(div);
+    }
+}
+
+
+function clickedOn () {
+    alert('yes');
+}
