67009125bc9e13a912c8f64afcb21b4a50fb312d
Robbie Gay <robbiecgay@gmail.com>
2019-09-26 11:32:11 -0400

    Can now create num1, including negative and dec point

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
 // JS
 
+// Array with all the button values
+// let calcBtns = ['C', '', '', '/', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '', '.', '='];
 let calcBtns = ['C', '', '', '/', 7, 8, 9, 'X', 4, 5, 6, '-', 1, 2, 3, '+', 0, '', '.', '='];
+// Default Values
 let calcAns = 0;
-let num1 = null;
-let num2 = null;
+let num1 = '';
+let num2 = '';
 let operand = '';
+let operation = 0;
 
+// Renders the page elements on load
 function loadCalc() {
+    // Function to render elements
     function renderElement(element, classes) {
         let output = document.createElement(element);
         output.className = classes;
@@ -17,20 +23,24 @@ function loadCalc() {
     let container = renderElement('div', 'container-fluid');
     let row = renderElement('div', 'row');
 
-    let leftCol = renderElement('div', 'col-0 col-sm-1 col-md-2 col-lg-3 bg-warning');
-    let centerCol = renderElement('div', 'col-12 col-sm-10 col-md-8 col-lg-6 text-center bg-success');
-    let rightCol = renderElement('div', 'col-0 col-sm-1 col-md-2 col-lg-3 bg-warning');
+    let leftCol = renderElement('div', 'col-0 col-sm-1 col-md-2 col-lg-3');
+    let centerCol = renderElement('div', 'col-12 col-sm-10 col-md-8 col-lg-6 text-center');
+    let rightCol = renderElement('div', 'col-0 col-sm-1 col-md-2 col-lg-3');
 
-    let title = renderElement('h1', 'my-5 display-4');
+    let title = renderElement('h1', 'my-5 display-4 text-white');
     title.innerHTML = 'Calculator';
 
-    let display = renderElement('div', 'col bg-white display-4 text-right');
+    let display = renderElement('div', 'col bg-light display-4 text-right');
+    display.setAttribute('style', 'height: 80px;');
     display.innerHTML = `${calcAns}`;
+
+    let bottom = renderElement('div', 'p-5');
 
     // Append Elements
     centerCol.appendChild(title);
     centerCol.appendChild(display);
-    createBoard(20);
+    createCalc(20);
+    centerCol.appendChild(bottom);
 
     row.appendChild(rightCol);
     row.appendChild(centerCol);
@@ -41,42 +51,148 @@ function loadCalc() {
     let app = document.getElementById('app');
     app.appendChild(container);
 
-    function createBoard(gridSize) {
+    // Creates the calculator
+    function createCalc(gridSize) {
+        let row = document.createElement('div');
+        row.className = 'row';
         for (let i = 0; i < gridSize; i++) {
-            let div = document.createElement('div');
-            div.className = 'col-3 border bg-white display-4';
-            div.id = `btn-${calcBtns[i]}`;
-            div.setAttribute('style', 'height: 80px;');
+            let btn = document.createElement('button');
+            btn.className = 'col-3 border bg-light display-4 button';
+            btn.setAttribute('type', 'button');
+            btn.id = `btn-${calcBtns[i]}`;
+            btn.setAttribute('style', 'height: 80px;');
             let text = document.createTextNode(`${calcBtns[i]}`);
-            div.appendChild(text);
+            btn.appendChild(text);
             if (calcBtns[i] !== '') {
-                div.addEventListener('click', clickedOn);
+                btn.addEventListener('click', clickedOn);
+            } else {
+                // Disables the blank buttons
+                btn.disabled = true;
             }
-            centerCol.appendChild(div);
+            row.appendChild(btn);
+            centerCol.appendChild(row);
+
+            // Click function
             function clickedOn() {
-                if (typeof calcBtns[i] === 'number') {
-                    if (num1 === null) {
-                        num1 = calcBtns[i];
-                    } else {
-                        num2 = calcBtns[i];
-                    }
-                } else if (calcBtns[i] === '=') {
-                    alert('You clicked =');
-                } else if (calcBtns[i] === '.') {
-                    alert('You clicked .');
-                } else if (calcBtns[i] === 'C') {
-                    num1 = null;
-                    num2 = null;
-                    operand = null;
+                // Clear Button
+                if (calcBtns[i] === 'C') {
+                    num1 = '';
+                    num2 = '';
+                    operand = '';
                     calcAns = 0;
-                } else {
-                    operand = calcBtns[i];
+                    operation = 0;
                 }
-                console.log(num1, operand, num2);
-                num1 = calcBtns[i];
-                display.innerHTML = `${calcAns}`;
+                // The first number
+                if (operation === 0) {
+                    // Can make the first number negative, if nothing else is inputted yet
+                    if (calcBtns[i] === '-' && num1 === '') {
+                        num1 += '-';
+                        console.log(num1);
+                    }
+                    // If the number doesn't have a decimal yet, add one
+                    else if (calcBtns[i] === '.' && !num1.includes('.')) {
+                        num1 += '.';
+                        console.log(num1);
+                    }
+                    // If it's a number, concatenate it
+                    else if (typeof calcBtns[i] === 'number') {
+                        num1 += String(calcBtns[i]);
+                        console.log(num1);
+                    }
+                    else if (calcBtns[i] === '+' || calcBtns[i] === '-' || calcBtns[i] === '/' || calcBtns[i] === 'X') {
+                        operation = 1;
+                        operand = calcBtns[i];
+                    }
+                }
+                if (operation === 1) {
+                    if (calcBtns[i] === '+' || calcBtns[i] === '-' || calcBtns[i] === '/' || calcBtns[i] === 'X') {
+                        operand = calcBtns[i];
+                    } else {
+                        operation = 2;
+                    }
+                }
+                if (operation === 2) {
+                    if (calcBtns[i] === '.' && !num1.includes('.')) {
+                        num1 += '.';
+                        console.log(num1);
+                    }
+                    // If it's a number, concatenate it
+                    else if (typeof calcBtns[i] === 'number') {
+                        num1 += String(calcBtns[i]);
+                        console.log(String(calcBtns[i]));
+                    } else if (calcBtns[i] === '+') {
+                        operation = 0;
+                        calcAns = Number(num1) + Number(num2);
+                    } else if (calcBtns[i] === '-') {
+                        operation = 0;
+                        calcAns = Number(num1) - Number(num2);
+                    } else if (calcBtns[i] === '/') {
+                        operation = 0;
+                        calcAns = Number(num1) / Number(num2);
+                    } else if (calcBtns[i] === 'X') {
+                        operation = 0;
+                        calcAns = Number(num1) * Number(num2);
+                    }
+                }
+                display.innerHTML = `${Number(num1)}`;
+                console.log(num1);
+                console.log(calcAns);
             }
         }
     }
-
 }
+
+
+// else if (calcBtns[i] === '=') {
+//     // Equals
+//     // If nothing after operand, should apply to self
+//     display.innerHTML = `${calcAns}`;
+// } else if (calcBtns[i] === 'C') {
+//     // Clear
+//     num1 = null;
+//     num2 = null;
+//     operand = null;
+//     calcAns = 0;
+
+
+
+
+
+
+// function clickedOn() {
+//     if (typeof calcBtns[i] === 'number') {
+//         // Numbers
+//         if (num1 === null) {
+//             // Can make the first number negative
+//             if (calcBtns[i] === '-') {
+//                 num1 += '-';
+//             } 
+//             // If the number doesn't have a decimal yet, add one
+//             else if (calcBtns[i] === '.' && !num1.includes('.')) {
+//                 num1 += '.';
+//             } else if (calcBtns[i] === '1') {
+//                 num1 += calcBtns[i];
+//             }
+//         } else {
+//             num2 = calcBtns[i];
+//         }
+//     } else if (calcBtns[i] === '=') {
+//         // Equals
+//         // If nothing after operand, should apply to self
+//         display.innerHTML = `${calcAns}`;
+//     }  else if (calcBtns[i] === 'C') { 
+//         // Clear
+//         num1 = null;
+//         num2 = null;
+//         operand = null;
+//         calcAns = 0;
+//     } else { 
+//         // Operands
+//         operand = calcBtns[i];
+//     }
+//     // Updates displays
+//     // console.log(num1, operand, num2);
+//     // Displays the button pressed
+//     display.innerHTML = `${Number(num1)}`;
+// }
+
